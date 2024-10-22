3428dacf96c12b01363436786a805b535b30ebbc
Manpreet Singh <68460299+I-am-Manu-Singh@users.noreply.github.com>
2024-10-22 22:47:32 +0530

    Time: 14 ms (14.12%), Space: 45.5 MB (81.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i< nums.length;i++)
         {
             if(map.containsKey(nums[i]))
-                map.replace(nums[i],map.get(nums[i]),map.get(nums[i])+2);
+                map.replace(nums[i],map.get(nums[i]),map.get(nums[i])+1);
             else
                 map.put(nums[i],1);
         }
