bf2ad4ef8e2c4e3f9810ffdf6ad748c468ad77d2
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2024-12-06 05:59:45 +0530

    Time: 15 ms (64.25%), Space: 45.8 MB (22.61%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean canChange(String start, String target) {
         int first = 0, second = 0, n = start.length();
-        
-        while (first < n || second < n) {
-            while (first < n && start.charAt(first) == '_')
-                first++;
-            while (second < n && target.charAt(second) == '_')
-                second++;
-            
-            if (first == n || second == n)
-                break;
-            if (start.charAt(first) != target.charAt(second))
-                return false;
-            if (start.charAt(first) == 'L' && first < second)
-                return false;
-            if (start.charAt(first) == 'R' && first > second)
-                return false;
-            
+
+
+        while(first < n || second < n) {
+            while(first < n && start.charAt(first) == '_')
+            first++;
+
+        while(second < n && target.charAt(second) == '_')
+            second++;   
+
+            if(first == n || second == n)
+            break;
+
+            if(start.charAt(first) != target.charAt(second))
+            return false;
+            if(start.charAt(first) == 'L' & first < second)  
+            return false;
+            if(start.charAt(first) == 'R' & first > second)
+            return false;   
+                    
             first++;
             second++;
+                    
         }
-        
         return first == n && second == n;
+
     }
 }
