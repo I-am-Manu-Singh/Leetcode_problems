52b17944cc731cde364d9374b769cbf54c0f5e40
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2024-12-06 05:50:08 +0530

    Time: 14 ms (82.12%), Space: 45.8 MB (29.15%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,31 +1,26 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        Stack<Pair> l=new Stack<>();
-        for(int i=start.length()-1;i>=0;i--){
-            if(start.charAt(i)=='L')l.add(new Pair(i,'L'));
-            else if(start.charAt(i)=='R')l.add(new Pair(i,'R'));
+        int i = 0, j = 0, n = start.length();
+        
+        while (i < n || j < n) {
+            while (i < n && start.charAt(i) == '_')
+                i++;
+            while (j < n && target.charAt(j) == '_')
+                j++;
+            
+            if (i == n || j == n)
+                break;
+            if (start.charAt(i) != target.charAt(j))
+                return false;
+            if (start.charAt(i) == 'L' && i < j)
+                return false;
+            if (start.charAt(i) == 'R' && i > j)
+                return false;
+            
+            i++;
+            j++;
         }
-        for(int i=0;i<target.length();i++){
-            char ch=target.charAt(i);
-            if(ch=='_')continue;
-            if(l.isEmpty())return false;
-            Pair p=l.peek();
-            if(ch=='L'){
-                if(p.a>=i&&p.c=='L')l.pop();
-                else return false;
-            }else if(ch=='R'){
-                if(p.a<=i&&p.c=='R')l.pop();
-                else return false;
-            }
-        }   
-        return l.isEmpty(); 
-    }
-    class Pair{
-        int a;
-        char c;
-        Pair(int a,char c){
-            this.a=a;
-            this.c=c;
-        }
+        
+        return i == n && j == n;
     }
 }
