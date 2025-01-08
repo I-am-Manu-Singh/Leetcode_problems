e82f73adaeea0b13005bdc7b2c7d6428797ae6f7
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2025-01-08 05:49:17 +0530

    Time: 0 ms (100%), Space: 44.5 MB (45.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,24 +1,17 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null||head.next==null){
-            return false;
-        }
-        ListNode fast=head;
-        ListNode slow=head;
+        ListNode fast = head;
+        ListNode slow = head;
 
-        /* we assume that cycle DNE ,i.e we have a singly linked list which will
-         end somewhere and if in between the fast and slow pointers met that
-        means there is no end to this linked list and cycle does exists*/
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
 
-        while(fast!=null&&fast.next!=null){ // covering both even and odd number of nodes,for even fast would reach null and for odd fast.next would be null
-
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                return true; //cycle does exists 
+            if (fast == slow) {
+                return true;
             }
         }
-        return false; // cycle DNE
-        
+
+        return false;        
     }
 }
