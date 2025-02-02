33285176c74029c9fae65d1a455095fd418b1d3e
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2025-02-02 08:54:35 +0530

    Time: 0 ms (100%), Space: 43.5 MB (32.64%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int len = nums.length;
+        if(len == 1)
+        {
+            return true;
+        }
+        for(int i = 1; i < len; i++)
+        {
+            if((nums[i] % 2) == (nums[i-1] % 2))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
