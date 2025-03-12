cdb9c95371b7fcedf169b5da8349ffdde1312e38
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2025-03-12 07:43:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,38 +1,38 @@
 class Solution {
     public long countOfSubstrings(String word, int k) {
-        return atLeast(k, word) - atLeast(k+1, word);
+       return atLeastOneConsonant(k, word - atLeast(k + 1, word)); 
     }
 
     public boolean isConsonant(char ch) {
-        return (ch!='a' && ch!='e'&& ch!='i'&& ch!='o'&& ch!='u');
+       return (ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u' );
     }
 
     public boolean allVowelsPresent (int freq[]) {
         return (freq['a'-'a'] > 0 && freq['e'-'a'] > 0 && freq['i'-'a'] > 0 && freq['o'-'a'] > 0 && freq['u'-'a'] > 0);
     }
 
-    public long atLeast(int k, String words) {
-            long count = 0;
-            int currentConsonant = 0;
-            int freq[] = new int [26];
-            int n = words.length();
-            int left = 0;
-            for(int right = 0; right < n; right++) {
-                char ch = words.charAt(right);
-                if(isConsonant(ch)){
-                    currentConsonant++;
-                }
-                freq[ch-'a']++;
-                while(currentConsonant>=k && allVowelsPresent(freq)) {
-                  count += (n-right);
-                   char character = words.charAt(left);
-                if(isConsonant(character)){
-                    currentConsonant--;
-                }
-                freq[character-'a']--;
-                left++;
-                }
+    public long atLeastOneConsonant(int k, String words) {
+        long count = 0;
+        int currentConsonant = 0;
+        int freq[] = new int [26];
+        int n = words.length();
+        int left = 0;
+        for(int right = 0; right < n; right++) {
+            char ch = words.charAt(right);
+            if(isConsonant(ch)) {
+                currentConsonant++;
             }
-        return count;    
+            freq[ch-'a']++;
+            while(currentConsonant >= k && allVowelsPresent(freq)) {
+                count += n-right;
+                char charac = words.charAt(left);
+            if(isConsonant(charac)){
+                currentConsonant--;
+            }
+            freq[charac-'a']--;
+            left++;
+            }
+        }
+        return count;
     }
 }
